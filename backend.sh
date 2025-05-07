d60f9d5096ccb0c3b337c06dbf4ea18f0059ebe4
swapna <swapnasiri.com>
2025-05-07 22:23:49 +0530

    conversatin added

--- backend.sh
+++ backend.sh
@@ -73,7 +73,7 @@ cp /home/ec2-user/expense-shell-scripting/backend.service /etc/systemd/system/ba
 dnf install mysql -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h mysql.daws82s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+mysql -h mysql.sdaws82s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
 VALIDATE $? "Setting up the transactions schema and tables"
 
 systemctl daemon-reload &>>$LOG_FILE_NAME
